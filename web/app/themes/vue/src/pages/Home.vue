540fece13d52ca263ffbff8bd3ebbabf8f5dfcc0
Vadim Mishkhozhev <vadim.stug@gmail.com>
2020-11-04 22:37:07 +0700

    Add apollo

--- web/app/themes/vue/src/pages/Home.vue
+++ web/app/themes/vue/src/pages/Home.vue
@@ -1,3 +1,34 @@
 <template>
+  <div>
     <div>Home</div>
+    <ul>
+      <li
+        v-for="post in posts.nodes"
+        :key="post.id"
+      >
+        {{ post.title }}
+      </li>
+    </ul>
+  </div>
 </template>
+
+<script>
+import gql from 'graphql-tag'
+
+export default {
+  apollo: {
+    posts: gql`
+        query {
+          posts {
+            nodes {
+              date
+              excerpt
+              id
+              title
+            }
+          }
+        }
+    `
+  }
+}
+</script>
