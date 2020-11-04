54ad32cd0dabba16e786b95b61176c5de0d28566
Vadim Mishkhozhev <vadim.stug@gmail.com>
2020-11-04 11:51:03 +0700

    Chnage server promise

--- web/app/themes/vue/src/entry-server.js
+++ web/app/themes/vue/src/entry-server.js
@@ -2,14 +2,25 @@
 import { createApp } from './app'
 import renderVueComponentToString from 'vue-server-renderer/basic';
 
-const { app, router, store } = createApp()
+new Promise((resolve, reject) => {
+    const { app, router, store } = createApp()
 
-router.push(context.url)
-store.commit('site/setMenus', context.menus)
+    router.push(context.url)
+    store.commit('site/setMenus', context.menus)
 
-renderVueComponentToString(app, (err, html) => {
-    if (err) {
-        throw new Error(err);
-    }
-    dispatch(html);
+    router.onReady(() => {
+        const matchedComponents = router.getMatchedComponents();
+        if (!matchedComponents.length) {
+            return reject({ code: 404 });
+        }
+        resolve(app);
+    }, reject)
+}).then(app => {
+    renderVueComponentToString(app, (err, html) => {
+        dispatch(html);
+    })
+}).cache(err => {
+    throw new Error(err)
 })
+
+
